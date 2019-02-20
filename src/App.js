fb39dcbeb259d4fdb10f6173947cb789664997e0
unknown <estebanbotero1036@gmail.com>
2019-02-20 18:16:39 -0500

    montaje 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      classTarea: '',
+
     }
   }
   render() {
@@ -20,14 +22,50 @@ class App extends Component {
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li key={task.id}
+            className={task.done === true ? "done" : null} onClick={this.SelectTarea.bind(this, task.id)}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.GuardarTarea.bind(this)}>
+            <input type="text" className={this.state.classTarea} id="new-task" placeholder="Ingresa una tarea y oprime Enter" onChange={this.ObtenerTarea.bind(this)} value={this.state.newTask} />
           </form>
         </div>
       </div>
     )
+  }
+
+  ObtenerTarea(event){
+    this.setState({
+      newTask: event.target.value,
+      classTarea: ""
+    })
+  }
+
+  SelectTarea(id){
+     let index = this.state.tasks.findIndex(x=> x.id === id);
+     let done = this.state.tasks[index].done;
+     this.state.tasks[index].done = !done
+      this.setState({
+        tasks: this.state.tasks        
+      })
+  }
+
+  GuardarTarea(event){
+    event.preventDefault();
+    if (this.state.newTask.length > 0) {
+      this.setState({
+        tasks: this.state.tasks.concat({
+          id: this.state.tasks.length + 1,
+          name:this.state.newTask,
+          done: false
+        }),
+        newTask:""
+      })
+    }
+    else {
+      this.setState({
+        classTarea: "error"
+      })
+    }
   }
 }
 
